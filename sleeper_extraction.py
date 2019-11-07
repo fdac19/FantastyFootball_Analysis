eabf88a7ef0026ac74c3932e583fcb4316e70f0a
tonydnguyen7 <tonydnguyen7@Yahoo.com>
2019-11-06 20:08:17 -0500

    added code to get players height and weight via sportsreference API

--- sleeper_extraction.py
+++ sleeper_extraction.py
@@ -20,9 +20,15 @@ for file in sys.argv[1:]:
             for line in sleepers:
                 key,value,player_id = re.split(': |, ', line)
                 sleepers_2019[key].append(value)
-            pp.pprint(sleepers_2019)
+                
+                player = Player(str(player_id).rstrip()) # strip the trailing newline after getting player_id from txt file
+                if player != None:
+                   print("player: %-20s Height: %-10s Weight: %-3dlbs" %(value, player.height, player.weight)) 
+
+            #pp.pprint(sleepers_2019)
             with open('sleepers2019.json', 'w') as f:
                 json.dump(sleepers_2019, f, indent=4)
+
         elif file == "SleeperList_2018.txt":
             for line in sleepers:
                 key,value,player_id = re.split(': |, ', line)
