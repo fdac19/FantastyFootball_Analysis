1ac62fdda92081b9c6472f3ac27f5f14a19e98bd
APoolio <andrewspooelap@gmail.com>
2019-11-24 11:07:26 -0500

    Added func for specific position desired

--- traversePlayerData.py
+++ traversePlayerData.py
@@ -1,5 +1,14 @@
 import json
 import sys
+from enum import Enum
+import numpy as np
+import matplotlib.pyplot as plt
+
+class position(Enum):
+    QB = 0
+    RB = 2
+    WR = 4
+    TE = 6
 
 #Sleepers list of players by position
 f = open('sleepers2019.json')
@@ -11,20 +20,37 @@ d = open('data.json')
 fantasyPointData = json.load(d)
 d.close
 
-#Options: QB, RB, TE, WR, DEF
+#Options: QB, RB, TE, WR, ALL
 desiredPosition = sys.argv[1]
+
+slotSearch = 'ALL'
+
+if desiredPosition == 'QB':
+    slotSearch = 'QuarterBack'
+if desiredPosition == 'RB':
+    slotSearch = 'RunningBack'
+if desiredPosition == 'WR':
+    slotSearch = 'WideReciever'
+if desiredPosition == 'TE':
+    slotSearch = 'TightEnd'
 
 for pos, player in data.items():
     sleeperListLength = len(player)
     print(pos)
 
-    for i in range(sleeperListLength):
-        if player[i] in fantasyPointData:
-            currPlayer = fantasyPointData.get(player[i])
-            statsLength = len(currPlayer['stats'])
+    if(pos == slotSearch or slotSearch == 'ALL'):
+        for i in range(sleeperListLength):
+            if player[i] in fantasyPointData:
+                currPlayer = fantasyPointData.get(player[i])
+                print(player[i])
+                statsLength = len(currPlayer['stats'])
+ 
+                for x in range(statsLength):
+                    playerProjectedPoints = currPlayer['stats'][x]['projected points']
+                    playerActualPoints = currPlayer['stats'][x]['actual points']
+                    weekProj = currPlayer['stats'][x]['week']
 
-            for x in range(statsLength):
-                playerProjectedPoints = currPlayer['stats'][x]['projected points']
-                playerActualPoints = currPlayer['stats'][x]['actual points']
-                weekProj = currPlayer['stats'][x]['week']
-                print(playerActualPoints)
+                    plt.plot(weekProj, playerProjectedPoints)
+
+
+plt.show()
